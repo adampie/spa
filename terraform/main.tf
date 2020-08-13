dfe34d6fe363a39f3d3280620a7ff5a595dcb675
Adam Pietrzycki <adam@tickb.com>
2020-08-14 00:45:53 +0100

    Clean up

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,28 +2,24 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
   comment = "oai"
 }
 
-# https://spa.adampie.co.uk
 module "dashboard" {
   source = "./modules/s3"
 
   bucket = "adampie-spa-dashboard"
 }
 
-# http://adampie-spa-s3.s3-website-us-east-1.amazonaws.com
 module "s3" {
   source = "./modules/s3"
 
   bucket = "adampie-spa-s3"
 }
 
-# http://adampie-spa-s3-acceleration.s3-accelerate.amazonaws.com/index.html
 module "s3_acceleration" {
   source = "./modules/s3_acceleration"
 
   bucket = "adampie-spa-s3-acceleration"
 }
 
-# https://spa-s3.adampie.co.uk
 module "s3_cloudfront" {
   source = "./modules/s3_cloudfront"
 
@@ -32,7 +28,6 @@ module "s3_cloudfront" {
   origin_access_identity_iam_arn  = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
 }
 
-# https://spa-s3a.adampie.co.uk
 module "s3_acceleration_cloudfront" {
   source = "./modules/s3_acceleration_cloudfront"
 
