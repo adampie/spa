9ccc587adef04e078d49c75ce78fe387b3dbe685
Adam Pietrzycki <adam@tickb.com>
2020-08-14 00:43:47 +0100

    Adding S3A Cloudfron

--- terraform/modules/s3_acceleration_cloudfront/main.tf
+++ terraform/modules/s3_acceleration_cloudfront/main.tf
@@ -26,3 +26,50 @@ data "aws_iam_policy_document" "s3_policy" {
     }
   }
 }
+
+resource "aws_cloudfront_distribution" "cloudfront" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = "s3"
+
+    s3_origin_config {
+      origin_access_identity = var.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = var.bucket
+  default_root_object = "index.html"
+  aliases             = ["spa-s3a.adampie.co.uk"]
+  price_class         = "PriceClass_All"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
