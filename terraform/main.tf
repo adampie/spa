075a8ce70df551e0c9b906c7a5ebba6533869c01
Adam Pietrzycki <adam@tickb.com>
2020-08-13 23:02:38 +0100

    Adding init cloudfront

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,3 +25,15 @@ module "s3_acceleration" {
 
   bucket = "adampie-spa-s3-acceleration"
 }
+
+module "s3_cloudfront" {
+  source = "./modules/s3_cloudfront"
+
+  bucket = "adampie-spa-s3-cloudfront"
+}
+
+module "s3_acceleration_cloudfront" {
+  source = "./modules/s3_acceleration_cloudfront"
+
+  bucket = "adampie-spa-s3-acceleration-cloudfront"
+}
