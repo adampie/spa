2ae00a9dba54bf8b46ec87d82a7307338ee4f0ed
Adam Pietrzycki <adam@tickb.com>
2020-08-13 21:59:30 +0100

    S3 + S3 Acceleration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,27 @@
-module "dashboard" {
+resource "aws_acm_certificate" "acm" {
+  domain_name               = adampie.co.uk
+  subject_alternative_names = ["*.adampie.co.uk"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+# module "dashboard" {
+#   source = "./modules/s3"
+
+#   bucket = "spa.adampie.co.uk"
+# }
+
+module "s3" {
+  source = "./modules/s3_acceleration"
+
+  bucket = "spa1.adampie.co.uk"
+}
+
+module "s3_acceleration" {
   source = "./modules/s3"
 
-  bucket = "spa.adampie.co.uk"
+  bucket = "spa2.adampie.co.uk"
 }
