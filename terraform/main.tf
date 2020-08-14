b167c20ab16cc3be6b8b8d1b8add165e333f585c
Adam Pietrzycki <adam@tickb.com>
2020-08-14 19:46:54 +0100

    Clean up terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,30 @@
+resource "aws_route53_zone" "dns" {
+  name = "singlepageapp.xyz"
+}
+
+resource "aws_acm_certificate" "acm" {
+  domain_name               = "singlepageapp.xyz"
+  subject_alternative_names = ["*.singlepageapp.xyz"]
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "validation" {
+  zone_id = aws_route53_zone.dns.zone_id
+  name    = aws_acm_certificate.acm.domain_validation_options.0.resource_record_name
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_acm_certificate.acm.domain_validation_options.0.resource_record_value]
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.acm.arn
+  validation_record_fqdns = aws_route53_record.validation.*.fqdn
+}
+
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
   comment = "oai"
 }
@@ -5,25 +32,25 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
 module "dashboard" {
   source = "./modules/s3"
 
-  bucket = "adampie-spa-dashboard"
+  bucket = "spa-dashboard"
 }
 
 module "s3" {
   source = "./modules/s3"
 
-  bucket = "adampie-spa-s3"
+  bucket = "spa-s3"
 }
 
 module "s3_acceleration" {
   source = "./modules/s3_acceleration"
 
-  bucket = "adampie-spa-s3-acceleration"
+  bucket = "spa-s3-acceleration"
 }
 
 module "s3_cloudfront" {
   source = "./modules/s3_cloudfront"
 
-  bucket                          = "adampie-spa-s3-cloudfront"
+  bucket                          = "spa-s3-cloudfront"
   cloudfront_access_identity_path = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
   origin_access_identity_iam_arn  = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
 }
