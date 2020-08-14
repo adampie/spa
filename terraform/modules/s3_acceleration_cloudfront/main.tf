2dc8836e24fc57855a00aee44fe2a2a26f013cae
Adam Pietrzycki <adam@tickb.com>
2020-08-14 01:00:00 +0100

    Replacing s3 with s3a?

--- terraform/modules/s3_acceleration_cloudfront/main.tf
+++ terraform/modules/s3_acceleration_cloudfront/main.tf
@@ -29,11 +29,13 @@ data "aws_iam_policy_document" "s3_policy" {
 
 resource "aws_cloudfront_distribution" "cloudfront" {
   origin {
-    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
-    origin_id   = "s3"
-
-    s3_origin_config {
-      origin_access_identity = var.cloudfront_access_identity_path
+    domain_name = "${var.bucket}.s3-accelerate.amazonaws.com"
+    origin_id   = "s3a"
+    custom_origin_config {
+      http_port              = "80"
+      https_port             = "443"
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1", "TLSv1.1", "TLSv1.2"]
     }
   }
 
@@ -46,7 +48,7 @@ resource "aws_cloudfront_distribution" "cloudfront" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "s3"
+    target_origin_id = "s3a"
 
     forwarded_values {
       query_string = false
