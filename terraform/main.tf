515e23ff066287ca657a5bd88ae1ac506bca3aa4
Adam Pietrzycki <adam@tickb.com>
2020-08-14 01:03:08 +0100

    Cloudfornt + S3a not a thing

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,11 +27,3 @@ module "s3_cloudfront" {
   cloudfront_access_identity_path = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
   origin_access_identity_iam_arn  = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
 }
-
-module "s3_acceleration_cloudfront" {
-  source = "./modules/s3_acceleration_cloudfront"
-
-  bucket                          = "adampie-spa-s3-acceleration-cloudfront"
-  cloudfront_access_identity_path = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
-  origin_access_identity_iam_arn  = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
-}
