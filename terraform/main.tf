7650a0a87c1d46eaa2305541e12028faa786c509
Adam Pietrzycki <adam@tickb.com>
2020-08-14 20:15:46 +0100

    Adding route53 validation back in

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,18 +12,18 @@ resource "aws_acm_certificate" "acm" {
   }
 }
 
-# resource "aws_route53_record" "validation" {
-#   zone_id = aws_route53_zone.dns.zone_id
-#   name    = aws_acm_certificate.acm.domain_validation_options.resource_record_name
-#   type    = "CNAME"
-#   ttl     = "300"
-#   records = [aws_acm_certificate.acm.domain_validation_options.resource_record_value]
-# }
+resource "aws_route53_record" "validation" {
+  zone_id = aws_route53_zone.dns.zone_id
+  name    = aws_acm_certificate.acm.domain_validation_options.*.resource_record_name
+  type    = "CNAME"
+  ttl     = "300"
+  records = [aws_acm_certificate.acm.domain_validation_options.*.resource_record_value]
+}
 
-# resource "aws_acm_certificate_validation" "cert" {
-#   certificate_arn         = aws_acm_certificate.acm.arn
-#   validation_record_fqdns = aws_route53_record.validation.*.fqdn
-# }
+resource "aws_acm_certificate_validation" "cert" {
+  certificate_arn         = aws_acm_certificate.acm.arn
+  validation_record_fqdns = aws_route53_record.validation.*.fqdn
+}
 
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
   comment = "oai"
