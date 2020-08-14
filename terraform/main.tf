124594e36acf013ea24dfc808252718f27d3b442
Adam Pietrzycki <adam@tickb.com>
2020-08-14 20:22:33 +0100

    wtf

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,16 +13,24 @@ resource "aws_acm_certificate" "acm" {
 }
 
 resource "aws_route53_record" "validation" {
-  zone_id = aws_route53_zone.dns.zone_id
-  name    = aws_acm_certificate.acm.domain_validation_options.resource_record_name
-  type    = "CNAME"
-  ttl     = "300"
-  records = [aws_acm_certificate.acm.domain_validation_options.resource_record_value]
+  for_each = {
+    for dvo in aws_acm_certificate.acm.domain_validation_options: dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.public_root_domain.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.acm.arn
-  validation_record_fqdns = aws_route53_record.validation.*.fqdn
+  validation_record_fqdns = [for record in aws_route53_record.validation: record.fqdn]
 }
 
 resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
