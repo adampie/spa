02f9cbf70a5182e6078e8499594537026d700479
Adam Pietrzycki <adam@tickb.com>
2020-08-14 20:23:46 +0100

    ???

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,7 +25,7 @@ resource "aws_route53_record" "validation" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = data.aws_route53_zone.public_root_domain.zone_id
+  zone_id         = data.aws_route53_zone.dns.zone_id
 }
 
 resource "aws_acm_certificate_validation" "cert" {
