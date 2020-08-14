210738db7b72904bca0d2592cb4f4035116c6666
Adam Pietrzycki <adam@tickb.com>
2020-08-14 19:55:17 +0100

    Updating terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,10 +14,10 @@ resource "aws_acm_certificate" "acm" {
 
 resource "aws_route53_record" "validation" {
   zone_id = aws_route53_zone.dns.zone_id
-  name    = aws_acm_certificate.acm.domain_validation_options.0.resource_record_name
+  name    = aws_acm_certificate.acm.domain_validation_options.resource_record_name
   type    = "CNAME"
   ttl     = "300"
-  records = [aws_acm_certificate.acm.domain_validation_options.0.resource_record_value]
+  records = [aws_acm_certificate.acm.domain_validation_options.resource_record_value]
 }
 
 resource "aws_acm_certificate_validation" "cert" {
@@ -32,25 +32,25 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
 module "dashboard" {
   source = "./modules/s3"
 
-  bucket = "spa-dashboard"
+  bucket = "spa-xyz-dashboard"
 }
 
 module "s3" {
   source = "./modules/s3"
 
-  bucket = "spa-s3"
+  bucket = "spa-xyz-s3"
 }
 
 module "s3_acceleration" {
   source = "./modules/s3_acceleration"
 
-  bucket = "spa-s3-acceleration"
+  bucket = "spa-xyz-s3-acceleration"
 }
 
 module "s3_cloudfront" {
   source = "./modules/s3_cloudfront"
 
-  bucket                          = "spa-s3-cloudfront"
+  bucket                          = "spa-xyz-s3-cloudfront"
   cloudfront_access_identity_path = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
   origin_access_identity_iam_arn  = aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn
 }
